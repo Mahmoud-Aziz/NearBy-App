9c4dd1f51d66247b15e487900ffa89dc07f25828
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2021-10-08 21:08:20 +0200

    Add main view model tests

---
+++ NearBy AppTests/MainViewModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewModelTests.swift
+//  NearBy AppTests
+//
+//  Created by Mahmoud Aziz on 08/10/2021.
+//
+
+import XCTest
+@testable import NearBy_App
+
+class MainViewModelTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+
+}
