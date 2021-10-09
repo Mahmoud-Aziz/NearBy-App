448aadd8f02131661132ff8ccf06c9c9f7562171
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2021-10-09 19:36:33 +0200

    Remove podfile.lock from .gitignore

---
+++ NearBy App/Constants/APISecret Constants.swift
@@ -0,0 +1,13 @@
+//
+//  APISecret Constants.swift
+//  NearBy App
+//
+//  Created by Mahmoud Aziz on 09/10/2021.
+//
+
+import Foundation
+
+struct APISecretConstant {
+    static let clientID = "P2ZU2QEY10AW4KEIBO2MZFLK40X5V2FNWKYWVRGIWH1RBIMH"
+    static let clientSecret = "AWSIUJMOKTE3YUKERJ325I2C4EUR0PFMJTPCI4O0MCAJVAEE"
+}
