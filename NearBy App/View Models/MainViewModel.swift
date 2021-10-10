1060cfe33935686889caa8642e1ae1434f599f24
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2021-10-10 22:57:21 +0200

    Enhance Main view controller
    - Now main view model include location manager delegate methods and is the delegate for the location manager.

--- NearBy App/View Models/MainViewModel.swift
+++ NearBy App/View Models/MainViewModel.swift
@@ -14,15 +14,18 @@ protocol MainViewModelProtocol {
     var showSpinner: (() -> ())? { get set }
     var hideSpinner: (() -> ())? { get set }
     var handleNetworkErrorViewModel: (() -> ())? { get set }
+    var handleError: (() -> ())? { get set }
+
     
     func configureLocationManager()
     func getNearbyPlaces(latitude: Double, longitude: Double)
     func placesCount() -> Int?
     func requestLocation()
     func populateCell(index: Int) -> CellViewModel
+    func setLocationManagerDelegate()
 }
 
-class MainViewModel {
+class MainViewModel: NSObject {
     var locationManager: CLLocationManager?
     private var places: [GroupItem] = []
     private var categories: [Category] = []
@@ -30,6 +33,8 @@ class MainViewModel {
     var showSpinner: (() -> ())?
     var hideSpinner: (() -> ())?
     var handleNetworkErrorViewModel: (() -> ())?
+    var handleError: (() -> ())?
+
 }
 
 //MARK: Main View Model Protocol Conformance
@@ -87,5 +92,32 @@ extension MainViewModel: MainViewModelProtocol {
         let shortName = place.venue.categories[0].shortName
         let cellViewModel = CellViewModel(name: name, address: address, id: id, placeCategory: shortName)
         return cellViewModel
+    }
+    
+    func setLocationManagerDelegate() {
+        locationManager?.delegate = self
+    }
+
+}
+
+extension MainViewModel: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last
+        guard let location: CLLocationCoordinate2D = lastLocation?.coordinate else { return }
+        let latitude = location.latitude
+        let longitude = location.longitude
+        print("locations = \(latitude) \(longitude)")
+        self.getNearbyPlaces(latitude: latitude, longitude: longitude)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if let error = error as? CLError, error.code == .denied {
+            manager.stopMonitoringSignificantLocationChanges()
+            print("Error fetching location \(error)")
+            self.handleError?()
+            return
+        }
+        self.handleError?()
     }
 }
